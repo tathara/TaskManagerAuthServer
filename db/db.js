953eb090b864b3f141158d8d30aeff911166af5e
tathara <sarturbusiness@gmail.com>
2024-01-16 18:51:27 +0300

    db connection update

--- db/db.js
+++ db/db.js
@@ -1,4 +1,12 @@
 import { Sequelize } from "sequelize";
 import pg from 'pg';
 
-export default new Sequelize(process.env.POSTGRES_URL, { dialect: 'postgres', dialectModule: pg });
+export default new Sequelize(process.env.POSTGRES_URL, {
+    dialect: 'postgres', dialectModule: pg, dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+
+    }
+});
