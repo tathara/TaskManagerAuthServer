b381974bfe23d060367b3a5db307ce5d1c33d276
tathara <sarturbusiness@gmail.com>
2024-01-17 17:12:36 +0300

    data dispatch to telegram fix

--- public/js/dispatcher.js
+++ public/js/dispatcher.js
@@ -3,16 +3,20 @@ import isValidData from "./validation.js";
 
 const tg = window.Telegram.WebApp;
 
+if (tg) {
+    console.log('Telegram.WebApp загружен!');
+}
+
 function dispatch(endpoint) {
     if (isValidData()) {
         const data = getData();
 
-        axios.post(endpoint, data).then(res => tg.sendData(res.data)).catch('Ошибка при выполнении POST запроса на эдпоинт ' + endpoint);
+        axios.post(endpoint, data).then(res => tg.sendData(res.data)).catch(error => console.log('Ошибка при выполнении POST запроса на эдпоинт ' + endpoint + '\n' + error));
     }
 }
 
 function getData() {
-    let data;
+    let data = {};
 
     const form = document.querySelector('form');
     const fields = form.querySelectorAll('input, select, textarea');
