4fbb474fd067092260e10977ee4b437423aec7fd
tathara <sarturbusiness@gmail.com>
2024-01-17 18:24:10 +0300

    submit button event listener fix

--- public/js/dispatcher.js
+++ public/js/dispatcher.js
@@ -1,12 +1,29 @@
 import isValidData from "./validation.js";
 
 const tg = window.Telegram.WebApp;
+const authorizationButton = document.getElementById('authorization');
+const registrationButton = document.getElementById('registration');
+
+if (authorizationButton) {
+    authorizationButton.addEventListener('click', () => dispatch('/authorization'));
+} else if (registrationButton) {
+    registrationButton.addEventListener('click', () => dispatch('/registration'));
+}
 
 if (tg) {
     console.log('Telegram.WebApp загружен!');
 }
 
-export default function dispatch(endpoint) {
+if (document.getElementById('authorization')) {
+    const submitButton = document.getElementById('authorization');
+    submitButton.addEventListener('click', dispatch('/authorization'));
+}
+else if (document.getElementById('registration')) {
+    const submitButton = document.getElementById('registration');
+    submitButton.addEventListener('click', dispatch('/registration'));
+}
+
+function dispatch(endpoint) {
     if (isValidData()) {
         const data = getData();
 
